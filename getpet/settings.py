b53a09fda2a8f22cf1a0a52b5e1d5ad8b77875f9
Karolis Vycius <k.vycius@gmail.com>
2018-11-17 00:17:25 +0200

    Remove import of celery

--- getpet/settings.py
+++ getpet/settings.py
@@ -15,8 +15,6 @@ import logging.config
 from django.utils.log import DEFAULT_LOGGING
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from celery.schedules import crontab
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
