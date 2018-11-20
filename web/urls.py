844e4184973c07f40581180d6ad066fa58652b95
Karolis Vycius <k.vycius@gmail.com>
2018-11-20 11:59:13 +0200

    Index page

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.views.generic import RedirectView
 
-from web.views import health_check
+from web.views import health_check, index
 
 urlpatterns = [
-    path('', RedirectView.as_view(url="https://play.google.com/store/apps/details?id=lt.getpet.getpet"), name="index"),
+    path('', index, name="index"),
     path('health/', health_check, name="health_check"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("OK")
+    return render(request, 'web/index.html')
 
 
 def health_check(request):
