789c07f2c8f70a8cb0f56fd15fdebb0c17ab7f8f
Karolis Vycius <k.vycius@gmail.com>
2020-04-17 20:52:21 +0300

    No shelter endpoint
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- management/urls.py
+++ management/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from management.views import ShelterPetsListView
+from management.views import ShelterPetsListView, no_associated_shelter
 
 urlpatterns = [
     path('', ShelterPetsListView.as_view(), name="management_pets_list"),
+    path('no-shelter/', no_associated_shelter, name="management_no_associated_shelter"),
 ]
