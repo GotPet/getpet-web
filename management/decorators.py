9785f26c0e33d20f4d6d15b25863ef3600be43ba
Karolis Vycius <k.vycius@gmail.com>
2020-04-18 15:50:56 +0300

    Remove django-enumfields
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- management/decorators.py
+++ management/decorators.py
@@ -1,9 +1,20 @@
-from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
+from django.contrib.auth.decorators import user_passes_test
+from django.urls import reverse
 
 from web.models import Shelter
 
-rec_associated_shelter_required = user_passes_test(lambda u: Shelter.user_selected_shelter(u) is not None)
 
-def associated_shelter_required(func=None, login_url=None):
+def associated_shelter_required(function=None, login_url=None):
+    """
+    Decorator for views that checks that the user is logged in and belongs to one of the shelters, redirecting
+    to the log-in page if necessary or no associated shelter view.
+    """
 
-    return login_required(rec_associated_shelter_required(), login_url=login_url)
+    actual_decorator = user_passes_test(
+        lambda u: u.is_authenticated and Shelter.user_selected_shelter(u) is not None,
+        login_url=login_url,
+        redirect_field_name='management_no_associated_shelter'
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
