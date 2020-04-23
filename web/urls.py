081662587263dbcdb4d7c38e22ac9e5460576811
Karolis Vycius <k.vycius@gmail.com>
2020-04-23 19:14:58 +0300

    Add favicon.ico
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from web.views import health_check, index
 
 urlpatterns = [
     path('', index, name="index"),
+    path('favicon.ico/', RedirectView.as_view(url='/static/favicon/favicon.ico', permanent=False)),
     path('health/', health_check, name="health_check"),
 ]
