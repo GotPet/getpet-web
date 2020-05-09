22f70c09610cbd17bff015bbbc2e9d157b7d5c45
Karolis Vycius <k.vycius@gmail.com>
2020-05-09 17:30:47 +0300

    Handle AssociateSheltersMiddleware cookie change in response
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- management/middleware.py
+++ management/middleware.py
@@ -1,6 +1,4 @@
-from django.http import HttpRequest
-
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 
 from management.constants import Constants
 from management.utils import try_parse_int
@@ -11,7 +9,7 @@ class AssociateSheltersMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, request: HttpRequest):
+    def __call__(self, request: HttpRequest) -> HttpResponse:
         from web.models import Shelter
 
         user = request.user
@@ -27,7 +25,11 @@ class AssociateSheltersMiddleware:
             if selected_shelter:
                 request.COOKIES[Constants.SELECTED_SHELTER_COOKIE_ID] = str(selected_shelter.id)
 
-        response = self.get_response(request)
+        response: HttpResponse = self.get_response(request)
+
+        # Cookie was changed in get_response
+        if response_shelter_cookie := response.cookies.pop(Constants.SELECTED_SHELTER_COOKIE_ID, None):
+            selected_shelter = Shelter.user_selected_shelter(user, shelter_id=response_shelter_cookie.value)
 
         if selected_shelter:
             response = selected_shelter.switch_shelter(response)
