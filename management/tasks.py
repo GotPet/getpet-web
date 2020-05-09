ff2b8898a097eb74ee902c9f6399196a6d816b58
Karolis Vycius <k.vycius@gmail.com>
2020-05-09 18:31:48 +0300

    Task connect_super_users_to_shelters
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- management/tasks.py
+++ management/tasks.py
@@ -1,6 +1,9 @@
 import logging
 
 from celery import shared_task
+from django.contrib.auth import get_user_model
+
+from web.models import Shelter
 
 logger = logging.getLogger(__name__)
 
@@ -8,3 +11,17 @@ logger = logging.getLogger(__name__)
 @shared_task(soft_time_limit=30)
 def test_task():
     return "Hello celery"
+
+
+@shared_task(soft_time_limit=30)
+def connect_super_users_to_shelters():
+    connected = 0
+    for user in get_user_model().objects.filter(is_superuser=True):
+        for shelter in Shelter.objects.exclude(authenticated_users=user):
+            shelter.authenticated_users.add(user)
+
+            connected += 1
+
+    return {
+        'users_connected': connected
+    }
