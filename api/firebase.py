ec248b55a2d59e508b314480685042bedf7bfda0
Karolis Vycius <k.vycius@gmail.com>
2020-05-11 11:00:56 +0300

    Initilize Firebase just once
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- api/firebase.py
+++ api/firebase.py
@@ -1,25 +1,22 @@
 import firebase_admin
-from django.utils.functional import SimpleLazyObject
-from firebase_admin import credentials
+from firebase_admin import auth, credentials
 
 from getpet import settings
-from firebase_admin import auth
 
 
 # noinspection PyMethodMayBeStatic
 class Firebase:
-    app = SimpleLazyObject(
-        lambda: firebase_admin.initialize_app(credentials.Certificate(settings.FIREBASE_KEY))
-    )
-
     def __new__(cls):
         if not hasattr(cls, 'instance') or not cls.instance:
             cls.instance = super().__new__(cls)
 
+            cred = credentials.Certificate(settings.FIREBASE_KEY)
+            firebase_admin.initialize_app(cred)
+
         return cls.instance
 
     def verify_id_token(self, id_token, check_revoked=False):
-        return auth.verify_id_token(id_token, app=self.app, check_revoked=check_revoked)
+        return auth.verify_id_token(id_token, check_revoked=check_revoked)
 
     def get_user(self, uid):
-        return auth.get_user(uid, app=self.app)
+        return auth.get_user(uid)
