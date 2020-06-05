d371a73016fb304fb15adfdc65dba4a312dc6fb1
Karolis Vycius <k.vycius@gmail.com>
2020-06-05 21:45:18 +0300

    Demo sections
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- web/urls.py
+++ web/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.views.generic import RedirectView
 
-from web.views import PetProfileView, health_check, index
+from web.views import IndexView, PetProfileView, health_check, index
 
 app_name = 'web'
 
 urlpatterns = [
     path('', index, name="index"),
+    path('demo/', IndexView.as_view(), name="demo"),
     path('pet/<int:pk>/', PetProfileView.as_view(), name="pet_profile"),
     path('favicon.ico/', RedirectView.as_view(url='/static/favicon/favicon.ico', permanent=False)),
     path('health/', health_check, name="health_check"),
--- web/views.py
+++ web/views.py
@@ -1,15 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
-from django.views.generic import DetailView
+from django.shortcuts import redirect
+from django.views.generic import DetailView, ListView
 
-from web.models import Pet
+from web.models import Pet, Shelter
 
 
 def index(request):
     if request.user.is_authenticated:
-        return render(request, 'web/index.html')
+        return redirect('web:demo')
 
     return redirect('https://www.facebook.com/getpet.lt/')
+
+
+class IndexView(ListView):
+    template_name = 'web/index.html'
+    model = Pet
+    context_object_name = 'pets'
+
+    def get_queryset(self):
+        shelter = Shelter.objects.filter(name='Linksmosios pėdutės').first()
+
+        pets = Pet.available.order_by('?')
+
+        if shelter is not None:
+            pets = pets.filter(shelter=shelter)
+
+        return pets[:6]
 
 
 class PetProfileView(DetailView):
