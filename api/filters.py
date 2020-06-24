5d402829944683e090752f26b05b3aae7da204b2
Karolis Vycius <k.vycius@gmail.com>
2020-06-24 09:29:52 +0300

    Filter pets also on shelter updated_at
    Signed-off-by: Karolis Vycius <k.vycius@gmail.com>

--- api/filters.py
+++ api/filters.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
 from web.models import Pet
@@ -5,7 +6,10 @@ from web.models import Pet
 
 class PetFilter(filters.FilterSet):
     pet_ids = filters.BaseInFilter(field_name="id", required=True)
-    last_update = filters.IsoDateTimeFilter(field_name="updated_at", lookup_expr='gt', required=False)
+    last_update = filters.IsoDateTimeFilter(field_name="updated_at", method='filter_last_update', required=False)
+
+    def filter_last_update(self, queryset, name, value):
+        return queryset.filter(Q(updated_at__gt=value) | Q(shelter__updated_at__gt=value))
 
     class Meta:
         model = Pet
